940adb1a1e64bb8a92f96d7d7d56ab1b5ec75422
stj <252742164@qq.com>
2023-02-13 14:56:03 +0800

    first commit

--- src/main/java/com/example/helloApplication2.java
+++ src/main/java/com/example/helloApplication2.java
@@ -3,6 +3,9 @@ package com.example;
 
 public class helloApplication2 {
     public static void main(String[] args) {
+
         System.out.println("test");
+        System.out.println("test");
+
     }
 }
